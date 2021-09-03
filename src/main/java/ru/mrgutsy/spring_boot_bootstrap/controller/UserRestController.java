763a44dc314630aec697fb1cc00d05b57fb9a16f
Daniil <mr.gutsy@bk.ru>
2021-09-03 14:36:21 +0300

    build

--- src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/UserRestController.java
+++ src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/UserRestController.java
@@ -1,7 +1,6 @@
 package ru.mrgutsy.spring_boot_bootstrap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.mrgutsy.spring_boot_bootstrap.model.Role;
@@ -27,37 +26,37 @@ public class UserRestController {
     @GetMapping("admin/allUsers")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("admin/{id}")
     public ResponseEntity<User> getUserById(@PathVariable long id) {
         User user = userService.getUserById(id);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("admin/authorities")
     public ResponseEntity<List<Role>> getAllRoles() {
         List<Role> roles = roleService.getAllRoles();
-        return new ResponseEntity<>(roles, HttpStatus.OK);
+        return ResponseEntity.ok(roles);
     }
 
     @PostMapping(value = "admin")
     public ResponseEntity<User> addNewUser(@RequestBody User user) {
         userService.addUser(user);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("admin")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         userService.updateUser(user);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("admin/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable long id) {
             userService.deleteUserById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 }
 
