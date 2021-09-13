47359dd4afdfa1f7b83388bb060421df42c3a42a
Daniil <mr.gutsy@bk.ru>
2021-09-13 10:41:27 +0300

    add AdminController

--- src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/AdminController.java
+++ src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/AdminController.java
@@ -10,29 +10,22 @@ import ru.mrgutsy.spring_boot_bootstrap.service.RoleService;
 import ru.mrgutsy.spring_boot_bootstrap.service.UserService;
 
 @Controller
-public class UserController {
+public class AdminController {
 
     private final UserService userService;
     private final RoleService roleService;
 
     @Autowired
-    public UserController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping(value = "/login")
-    public String login() {
-        return "loginpage";
-    }
-
-    @GetMapping(value = "/user")
-    public String userInfo(@AuthenticationPrincipal User user, Model model) {
+    @GetMapping(value = "/admin")
+    public String listUsers(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("user", user);
-        model.addAttribute("roles", user.getRoles());
-        return "userpage";
+        model.addAttribute("allUsers", userService.getAllUsers());
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "adminpage";
     }
-
-
 }
-
--- src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/AdminRestController.java
+++ src/main/java/ru/mrgutsy/spring_boot_bootstrap/controller/AdminRestController.java
@@ -2,7 +2,14 @@ package ru.mrgutsy.spring_boot_bootstrap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.mrgutsy.spring_boot_bootstrap.model.Role;
 import ru.mrgutsy.spring_boot_bootstrap.model.User;
 import ru.mrgutsy.spring_boot_bootstrap.service.RoleService;
@@ -11,14 +18,15 @@ import ru.mrgutsy.spring_boot_bootstrap.service.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/")
-public class UserRestController {
+@RequestMapping(value = "/admin/")
+public class AdminRestController {
+
 
     private final UserService userService;
     private final RoleService roleService;
 
     @Autowired
-    public UserRestController(UserService userService, RoleService roleService) {
+    public AdminRestController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
@@ -55,8 +63,7 @@ public class UserRestController {
 
     @DeleteMapping("admin/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable long id) {
-            userService.deleteUserById(id);
+        userService.deleteUserById(id);
         return ResponseEntity.ok().build();
     }
 }
-
